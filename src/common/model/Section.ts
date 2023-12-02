a7d4cde9c14a68ee7c059c851ed4044ddf94c610
eoc20599 <pietro.balestra@eoc.ch>
2023-12-02 14:11:13 +0100

    Section element non nullable

--- src/common/model/Section.ts
+++ src/common/model/Section.ts
@@ -2,7 +2,7 @@ export type Section = {
   key: string;
   order: number;
   height: number;
-  element: JSX.Element | null;
+  element: JSX.Element;
 };
 
 export type Sections = Array<Omit<Section, "order">>;

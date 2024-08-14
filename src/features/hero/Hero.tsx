d710a2a2d69062e89b19b92b09146422af2dca7f
eoc20599 <pietro.balestra@eoc.ch>
2024-08-15 00:23:29 +0200

    wip - portrait and landscape hero

--- src/features/hero/Hero.tsx
+++ src/features/hero/Hero.tsx
@@ -1,52 +1,16 @@
-import { useEffect, useRef } from "react";
-import "./Hero.css";
-import after from "./assets/20231103_Portfolio_ragazzo_square_DOPO.jpg";
-import before from "./assets/20231103_Portfolio_ragazzo_square_PRIMA.jpg";
+import { useMediaQuery } from "@mui/material";
+import HeroPortrait from "./HeroPortrait";
+import HeroLandscape from "./HeroLandscape";
 
 const Hero = () => {
-  const imagesContainerRef = useRef<HTMLDivElement>(null);
-  const beforeImageRef = useRef<HTMLDivElement>(null);
+  const match = useMediaQuery("(max-width: 600px)");
+  console.log("match:", match);
 
-  useEffect(() => {
-    const beforeImage = beforeImageRef.current;
-    const imagesContainer = imagesContainerRef.current;
-    if (!beforeImage || !imagesContainer) return;
+  if (match) {
+    return <HeroPortrait />;
+  }
 
-    const handleScroll = () => {
-      const scrollPercent = window.scrollY / window.innerHeight;
-
-      if (scrollPercent < 1) {
-        beforeImage.style.height = `${100 - scrollPercent * 100}vh`;
-        imagesContainer.style.position = "fixed";
-        imagesContainer.style.top = "40px";
-      } else {
-        beforeImage.style.height = `0`;
-        imagesContainer.style.position = "absolute";
-        imagesContainer.style.top = "calc(100vh + 40px)";
-      }
-    };
-
-    handleScroll();
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  return (
-    <div className="hero">
-      <div ref={imagesContainerRef} className="image-container">
-        <div ref={beforeImageRef} className="before-image">
-          <img src={before} alt="Before" />
-        </div>
-        <div className="after-image">
-          <img src={after} alt="After" />
-        </div>
-      </div>
-      <div className="placeholder" />
-    </div>
-  );
+  return <HeroLandscape />;
 };
 
 export default Hero;
--- src/features/hero/HeroPortrait.tsx
+++ src/features/hero/HeroPortrait.tsx
@@ -1,73 +1,64 @@
-import { ImgComparisonSlider } from "@img-comparison-slider/react";
+import { useEffect, useRef } from "react";
+import "./HeroPortrait.css";
 import after from "./assets/20231103_Portfolio_ragazzo_square_DOPO.jpg";
 import before from "./assets/20231103_Portfolio_ragazzo_square_PRIMA.jpg";
-import { useEffect, useState } from "react";
+import { useMediaQuery } from "@mui/material";
 
 const HeroPortrait = () => {
-  const [section, setSection] = useState({
-    scroll: 0,
-    isSticky: false,
-    top: 0,
-  });
+  const imagesContainerRef = useRef<HTMLDivElement>(null);
+  const beforeImageRef = useRef<HTMLDivElement>(null);
+
+  const matches = useMediaQuery("(max-width: 768px)");
 
   useEffect(() => {
-    const onScroll = () => {
-      // scroll must be offset by 100vh
+    const beforeImage = beforeImageRef.current;
+    const imagesContainer = imagesContainerRef.current;
+    if (!beforeImage || !imagesContainer) return;
 
-      const scroll = window.scrollY / window.innerHeight - 1;
-      const isSticky = scroll <= 2;
-      const top = scroll <= 1 ? 0 : -(scroll - 1) * window.innerHeight;
+    const handleScroll = () => {
+      const scrollPercent = window.scrollY / (window.innerHeight - 40);
+      console.log("scrollPercent:", scrollPercent);
 
-      if (scroll > 2) {
-        setSection({
-          scroll: 2,
-          isSticky: false,
-          top: -window.innerHeight,
-        });
-
-        return;
+      if (scrollPercent < 1) {
+        beforeImage.style.height = `100vh`;
+        imagesContainer.style.position = "absolute";
+        imagesContainer.style.top = "100vh";
+      } else if (scrollPercent < 2) {
+        const relativeScroll = scrollPercent - 1;
+        beforeImage.style.height = `${100 - relativeScroll * 100}vh`;
+        imagesContainer.style.position = "fixed";
+        imagesContainer.style.top = "40px";
+      } else {
+        beforeImage.style.height = `0`;
+        imagesContainer.style.position = "absolute";
+        imagesContainer.style.top = "calc(200vh - 40px)";
       }
-
-      setSection({
-        scroll,
-        isSticky,
-        top,
-      });
     };
 
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [matches]);
 
   return (
-    <>
-      <div className="hero-column-1">
-        <div className="hero-title-wrapper">
-          <h1 className="hero-title">Revive Your Memories</h1>
-          <p className="hero-subtitle">Preserving the Past, Enhancing the Present</p>
-          <a href="#contacts" className="hero-button">
-            Get Started
-          </a>
+    <div className="hero">
+      <div className="hero-title">
+        <h1>Before & After</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+      </div>
+      <div ref={imagesContainerRef} className="image-container">
+        <div ref={beforeImageRef} className="before-image">
+          <img src={before} alt="Before" />
+        </div>
+        <div className="after-image">
+          <img src={after} alt="After" />
         </div>
       </div>
-      <div
-        className="hero-portrait gray"
-        style={{
-          position: section?.isSticky ? "sticky" : "relative",
-          top: section?.top,
-          // zIndex: 2,
-        }}
-      >
-        <div className="hero-column-2">
-          <ImgComparisonSlider value={(1 - (section?.scroll ?? 0)) * 100} direction={"vertical"} className="comparator" handle={true}>
-            <img alt="first" slot="first" src={before} className="hero-image-to-compare" />
-            <img alt="second" slot="second" src={after} className="hero-image-to-compare" />
-          </ImgComparisonSlider>
-        </div>
-      </div>
-      <section />
-      <section className="placeholder-section" />
-    </>
+      <div className="placeholder" />
+    </div>
   );
 };
 
